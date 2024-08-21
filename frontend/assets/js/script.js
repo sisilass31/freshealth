5b640e7e05aed8023ac006a937da0912a41989f4
sisilass31 <lassarsihem31@gmail.com>
2024-08-21 04:27:29 +0200

    delete/ faq

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -13,20 +13,4 @@ function toggleSidebar() {
 
 /* FAQ */
 
-document.querySelectorAll('.faq-question').forEach(question => {
-    question.addEventListener('click', () => {
-        const answer = question.nextElementSibling;
-        const arrow = question.querySelector('.arrow');
-
-        // Toggle the answer's visibility
-        if (answer.style.display === 'block') {
-            answer.style.display = 'none';
-            arrow.classList.remove('rotated');
-        } else {
-            answer.style.display = 'block';
-            arrow.classList.add('rotated');
-        }
-    });
-});
-
 /* FAQ */
