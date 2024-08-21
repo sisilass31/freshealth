f82d2b70b39b95d2824c40887686fb7f82e8219c
sisilass31 <lassarsihem31@gmail.com>
2024-08-22 01:06:58 +0200

    fix/ modal-need to fix again

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -2,6 +2,31 @@
 
 /* recettes */
 
+/* pop up recette */
+
+// Script pour gérer l'affichage du modal
+const modal = document.getElementById("myModal");
+const btns = document.querySelectorAll(".buttonrecipevoirplus");
+const span = document.getElementsByClassName("close-popup")[0];
+
+btns.forEach(btn => {
+    btn.onclick = function () {
+        modal.style.display = "flex";
+    }
+});
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+/* pop up recette */
+
 /* navbar */
 
 function toggleSidebar() {
